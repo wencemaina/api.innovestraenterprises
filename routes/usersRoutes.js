4769f99e4889d93d524b45204378860855d2ed12
wencemaina <mainawence@gmail.com>
2025-05-08 07:26:00 +0300

    Add login route to usersRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,9 +5,12 @@ const router = express.Router();
 const { getAllUsers } = require("../controllers/getAllUsers");
 
 const { signUp } = require("../controllers/signupController");
+const { login } = require("../controllers/loginController");
 
 router.get("/all", getAllUsers);
 
 router.post("/user/register", signUp);
+
+router.post("user/login", login);
 
 module.exports = router;
