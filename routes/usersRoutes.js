a502ad09c4b03f8069db27668c53deb958c08236
wencemaina <mainawence@gmail.com>
2025-05-08 07:17:15 +0300

    Add user registration route to usersRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,6 +4,10 @@ const express = require("express");
 const router = express.Router();
 const { getAllUsers } = require("../controllers/getAllUsers");
 
+const { signUp } = require("../controllers/signupController");
+
 router.get("/all", getAllUsers);
+
+router.post("/user/register", signUp);
 
 module.exports = router;
