a028cc3a6a36bcfdc8f61469f2d41e91fc243054
wencemaina <mainawence@gmail.com>
2025-05-09 08:19:37 +0300

    Fix import path for checkAuthStatusController and add logout route to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { refreshToken } = require("../controllers/refreshTokenController");
-const { checkAuthStatus } = require("../controllers/checkAuthStatusController");
+const { checkAuthStatus } = require("../controllers/authStatusController");
+const { logout } = require("../controllers/logoutController");
 
 router.post("/refresh/token", refreshToken);
 
 router.post("/check/status", checkAuthStatus);
+
+router.post("/logout", logout);
 
 module.exports = router;
