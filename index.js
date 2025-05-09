132743c494d34ad67a444b308b57b492910de255
wencemaina <mainawence@gmail.com>
2025-05-09 18:32:19 +0300

    Remove unnecessary whitespace in commented-out server listen code

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-/*  app.listen(PORT, "0.0.0.0", () => {
+/* app.listen(PORT, "0.0.0.0", () => {
 	console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
  */
