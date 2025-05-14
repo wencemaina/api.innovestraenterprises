3f6141cd9531aba7a0b07f5a09970a6eb7f52e9f
wencemaina <mainawence@gmail.com>
2025-05-14 15:33:08 +0300

    Add routes for password reset functionality

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const { refreshToken } = require("../controllers/refreshTokenController");
 const { checkAuthStatus } = require("../controllers/authStatusController");
 const { logout } = require("../controllers/logoutController");
+const { requestPasswordReset } = require("../controllers/requestPasswordReset");
+const { resetPassword } = require("../controllers/resetPasswordController");
 
 router.post("/refresh/token", refreshToken);
 
 router.post("/check/status", checkAuthStatus);
 
 router.post("/logout", logout);
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
