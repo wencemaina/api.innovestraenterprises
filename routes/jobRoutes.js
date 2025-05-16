43e78e48cd804ad903131e6c780c2ab8f3fdef61
wencemaina <mainawence@gmail.com>
2025-05-16 05:32:14 +0300

    Add route for retrieving jobs specific to an employer

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -6,8 +6,12 @@ const upload = multer({ storage });
 const { createJob } = require("../controllers/createJobController");
 const { getAllJobs } = require("../controllers/getAllJobsController");
 
+const { getEmployerJobs } = require("../controllers/getEmployerJobsController");
+
 // Apply the multer middleware directly in the route
 router.post("/create/new-job", upload.any(), createJob);
 
 router.get("/all", getAllJobs);
+
+router.get("/employer-jobs", getEmployerJobs);
 module.exports = router;
