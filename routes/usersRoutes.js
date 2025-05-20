74db8a27f4e23b5c80a7eb33994f3bcb544fbf7a
wencemaina <mainawence@gmail.com>
2025-05-20 13:04:37 +0300

    Add user account retrieval and deletion routes to usersRoutes

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,7 +2,11 @@
 
 const express = require("express");
 const router = express.Router();
-const { getAllUsers } = require("../controllers/getAllUsers");
+const {
+	getAllUsers,
+	getCurrentUser,
+	deleteUser,
+} = require("../controllers/allUsersController");
 
 const { signUp } = require("../controllers/signupController");
 const { login } = require("../controllers/loginController");
@@ -12,5 +16,9 @@ router.get("/all", getAllUsers);
 router.post("/user/register", signUp);
 
 router.post("/user/login", login);
+
+router.get("/user/account", getCurrentUser);
+
+router.delete("/user/delete/:userId", deleteUser);
 
 module.exports = router;
