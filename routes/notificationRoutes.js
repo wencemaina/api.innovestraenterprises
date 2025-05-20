3c45b45f365e5a9d2223bff5824ad44bc0acc874
wencemaina <mainawence@gmail.com>
2025-05-20 03:51:40 +0300

    Add notification management routes for marking notifications as read

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -4,10 +4,19 @@ const router = express.Router();
 const {
 	getWriterNotifications,
 	getEmployerNotifications,
+	markNotificationAsRead,
+	markAllNotificationsAsRead,
+	clearAllNotifications,
 } = require("../controllers/notificationsController");
 
 router.get("/writer/all", getWriterNotifications);
 
 router.get("/employer/all", getEmployerNotifications);
+
+router.put("/:notificationId/read", markNotificationAsRead);
+
+router.put("/read-all", markAllNotificationsAsRead);
+
+router.delete("/clear-all", clearAllNotifications);
 
 module.exports = router;
